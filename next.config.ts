fcefbb3bcda331c071ffe00185d6ecd6a6dc6608
Dov <dovindustries@gmail.com>
2025-10-31 12:45:00 -0400

    Add www redirect to fix sitemap access

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,22 @@ const nextConfig: NextConfig = {
     ],
   },
 
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "dovindustries.com",
+          },
+        ],
+        destination: "https://www.dovindustries.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
+
   async headers() {
     return [
       {
